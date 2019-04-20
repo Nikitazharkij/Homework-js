d97d5ed62bacddbde9c3d3e6f9cc16f8318e8555
Nikita Zharkij <kakdelanik@mail.ru>
2019-04-20 12:39:23 +0300

    changed validator Added classes

--- 12/09-task/validator.js
+++ 12/09-task/validator.js
@@ -1,27 +1,38 @@
-function CommonValidator(msg) {
-    this.errorMessage = msg || 'Common error message';
-    this.toString = () => {
-        return this.errorMessage;
+'use strict';
+
+class CommonValidator {
+
+    constructor(msg) {
+        this.errorMessage = msg || 'Common error message';
+        this.toString = () => {
+            return this.errorMessage;
+        }
     }
 }
 
-function Required() {
-    CommonValidator.call(this, 'This field is required.');
-    this.test = value => {
-        return value.length > 0;
+class Required {
+    constructor() {
+        CommonValidator.call(this, 'This field is required.');
+        this.test = value => {
+            return value.length > 0;
+        }
     }
 }
 
-function MinLength(value) {
-    CommonValidator.call(this, 'Min length should be 7');
-    this.test = value => {
-        return value.length >= 7;
+class MinLength {
+    constructor(value) {
+        CommonValidator.call(this, 'Min length should be 7');
+        this.test = value => {
+            return value.length >= 7;
+        }
     }
 }
 
-function ContainSing(value) {
-    CommonValidator.call(this, 'Should be contained sing @');
-    this.test = function(value) {
-        return value.indexOf('@') !== -1;
+class ContainSing {
+    constructor(value) {
+        CommonValidator.call(this, 'Should be contained sing @');
+        this.test = value => {
+            return value.indexOf('@') !== -1;
+        }
     }
 }
